4f500c59c40c4deae100f568045703991ae42002
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 17:36:22 +0100

    fullstack notes with all lint errors fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const Note = require('./models/note')
 
 app.use(cors())
@@ -16,8 +15,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {    
-    return response.status(400).json({ error: error.message })  
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -26,8 +25,8 @@ const errorHandler = (error, request, response, next) => {
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
-  
-app.post('/api/notes', (request, response) => {
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -57,44 +56,45 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
-      if (note) {        
-        response.json(note)      
-      } else {        
-        response.status(404).end()      
-      }    
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
-  })
+})
 
-  app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-    Note.findByIdAndUpdate(
-      request.params.id, 
-      { content, important },    
-      { new: true, runValidators: true, context: 'query' } ) 
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' } )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 /*   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
-  
+
     response.status(204).end()
   }) */
-  app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  const PORT = process.env.PORT
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- models/note.js
+++ models/note.js
@@ -2,25 +2,27 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
+
 mongoose.connect(url)
-  .then(result => {    
-    console.log('connected to MongoDB')})  
-    .catch((error) => {    
-        console.log('error connecting to MongoDB:', error.message)  
-    })
+  .then(() => {
+    console.log('connected to MongoDB')})
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-  content: {    
-    type: String,    
-    minLength: 5,    
-    required: true  
-  },  
-  date: {     
-    type: Date,    
-    required: true  
-  },  
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
   important: Boolean
 })
 
