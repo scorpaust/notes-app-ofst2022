3f4823abebaa507585c8a0683af3abf417e86410
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 01:03:48 +0100

    fullstack notes with mongo db

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
+
+const password = encodeURIComponent(process.env.REACT_APP_MONGODB_PASS)
+
+console.log("pass: ", password)
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 
@@ -39,40 +56,32 @@ const generateId = () => {
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end()  
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -82,7 +91,7 @@ app.get('/', (request, response) => {
     response.status(204).end()
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT 
   
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
