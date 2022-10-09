78fb32379d12685257e4bd2b2f9f2cc5f8868659
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 12:35:48 +0100

    fullstack notes with mongo db and handling of validation errors via mongoose

--- index.js
+++ index.js
@@ -68,11 +68,11 @@ app.get('/api/notes/:id', (request, response, next) => {
 
   app.put('/api/notes/:id', (request, response, next) => {
     const { content, important } = request.body
-    
+
     Note.findByIdAndUpdate(
       request.params.id, 
       { content, important },    
-      { new: true, runValidators: true, context: 'query' }  ) 
+      { new: true, runValidators: true, context: 'query' } ) 
       .then(updatedNote => {
         response.json(updatedNote)
       })
