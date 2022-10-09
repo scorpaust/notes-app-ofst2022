41e66e0d67db477deded378fa6cab5d7f3803f99
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 11:14:57 +0100

    fullstack notes with mongo db and middleware error handling

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.use(express.static('build'))
 
 app.use(express.json())
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
   
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -38,11 +51,32 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {        
+        response.json(note)      
+      } else {        
+        response.status(404).end()      
+      }    
+    })
+    .catch(error => next(error))
   })
-})
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
 
 /*   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -50,6 +84,13 @@ app.get('/api/notes/:id', (request, response) => {
   
     response.status(204).end()
   }) */
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
 
   const PORT = process.env.PORT
   
