54d4cd163244e18bf7b69e5a4943b96a8b5c30b1
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 01:20:50 +0100

    fullstack notes with mongo db

--- index.js
+++ index.js
@@ -25,64 +25,37 @@ app.use(express.static('build'))
 
 app.use(express.json())
 
-let notes = [  
-    {    
-        id: 1,    
-        content: "HTML is hard",    
-        date: "2022-05-30T17:30:31.098Z",    
-        important: true  
-    },  
-    {    
-        id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2022-05-30T18:39:34.091Z",    
-        important: false  
-    },  
-    {    
-        id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2022-05-30T19:20:14.298Z",    
-        important: true  
-    }
-]
+  
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
-  
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    })
-  
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
   })
 
-  
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
+  note.save().then(savedNote => {
+    response.json(savedNote)
   })
+})
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
-    })
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
   })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
 
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
